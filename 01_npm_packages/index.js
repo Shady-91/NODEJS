2370d8e9433c36f5d39aea11a4c3e4d64f3a3aa0
Shady-91 <Shady_91@hotmail.it>
2025-09-15 21:55:09 +0200

    primo commit

---
+++ 01_npm_packages/index.js
@@ -0,0 +1,15 @@
+//initialize the project with npm init -y
+
+
+
+//create a file index.js in the project's root folder
+
+
+//add a console.log statement in index.js
+
+console.log('Hello, World!');
+//install the dependencies with npm install (npm i axios)
+
+//import axios inside your script
+
+//make a GET request using axios
